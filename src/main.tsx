695f73a22a08df0226bc158323f656b2cdb37dfa
lemonliu <lemonliu2023@163.com>
2024-07-02 13:44:54 +0800

    refactor: layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { Navigate, RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import Markdown from './views/Markdown/index.tsx';
 import Threejs from './views/Threejs/index.tsx';
 import OnlyView from './views/Markdown/OnlyView/index.tsx';
