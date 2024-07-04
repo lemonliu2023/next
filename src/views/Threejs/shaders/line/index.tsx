7f1237145338751855e5ab10c9954a8d58e04ce7
lemonliu <lemonliu2023@163.com>
2024-07-04 18:17:06 +0800

    feat: 增加管道

--- src/views/Threejs/shaders/line/index.tsx
+++ src/views/Threejs/shaders/line/index.tsx
@@ -1,26 +1,29 @@
-// import * as THREE from 'three';
-import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry.js';
-import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js';
-import { LineSegments2 } from 'three/examples/jsm/lines/LineSegments2.js';
-
+import * as THREE from 'three';
 const line = () => {
-  const material = new LineMaterial({
-    color: 'blue',
-    linewidth: 10,
-    // opacity: 1,
-    transparent: true,
-    depthTest: false,
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-  });
+  
+    const tubeGeometry = new THREE.TubeGeometry(new THREE.CatmullRomCurve3([new THREE.Vector3(0, 0, 0), new THREE.Vector3(100, 100, 100)]), 80, 0.4)
+    const texture = new THREE.TextureLoader().load('/next/public/tube_bg.png')
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping; //每个都重复
+    texture.repeat.set(1, 1)
+    texture.needsUpdate = true
+    const material = new THREE.MeshBasicMaterial({
+        map: texture,
+        side: THREE.BackSide,
+        transparent: true
+    })
+    const mesh = new THREE.Mesh(tubeGeometry, material);
+    const animationFn = () => {
+        console.log(material)
+        if(material.map?.offset?.x !== undefined) {
+            console.log(1111)
+            material.map.offset.x = material.map?.offset?.x - 0.01
+        }
+        requestAnimationFrame(animationFn)
+    }
+    animationFn()
+    console.log(mesh, 'mesh')
+    return mesh
 
-  const geometry = new LineGeometry();
-
-  geometry.setPositions([0, 0, 0, 100, 100, 100]);
-
-  const mesh = new LineSegments2(geometry, material);
-
-  return mesh;
 };
 
 export default line;
