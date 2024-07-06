ce35f93d4f4fca489cc115ecd1393d55600605ff
lemonliu <lemonliu2023@163.com>
2024-07-06 22:14:43 +0800

    build: useInitAll optimze

---
+++ docs/assets/clone-Cyudgv5A-DXcyB2ya.js
@@ -0,0 +1,1 @@
+import{j as n}from"./graph-Cx5EwEy7-bh7-ZvHV.js";var o=4;function a(r){return n(r,o)}export{a as n};
---
+++ docs/assets/index-CGvt6i8F.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-TXiiCiUg.js";function r(){return t.jsx(s,{})}export{r as default};
