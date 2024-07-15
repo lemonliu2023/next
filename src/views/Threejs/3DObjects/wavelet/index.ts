d3a66a815de7b715ffa0debc96762d8ddfd0045c
lemonliu <lemonliu2023@163.com>
2024-07-15 08:55:56 +0800

    feat: optimize wavelet

--- src/views/Threejs/3DObjects/wavelet/index.ts
+++ src/views/Threejs/3DObjects/wavelet/index.ts
@@ -15,7 +15,6 @@ varying vec2 vUv;
 uniform float uTime;  
 uniform vec2 uCenter;  
 uniform float uRadius;  
-uniform float uAmplitude;
 uniform vec3 u_color;
   
 void main() {  
@@ -23,8 +22,7 @@ void main() {
     vec2 dist = (vUv - uCenter);  
     float len = length(dist) * 2.0;  
 
-    float circle = mod(uTime, 1.0);
-    float circle2 = mod(uTime + 0.5, 1.0);
+    float circle = sin(0.5 * 3.1415926 * mod(uTime, 1.0));
 
     if(len < circle) {
         gl_FragColor = vec4(1.0, .0, .0, pow((1.0 - circle + len), 5.0) * (1.0 - len));
@@ -43,10 +41,9 @@ export default function (option = {}) {
         vertexShader: vertexShader,
         fragmentShader: fragmentShader,
         uniforms: {
-            uTime: { value: 0.0 }, // 初始时间值
+            uTime: { value: 0.25 }, // 初始时间值
             uCenter: { value: new THREE.Vector2(0.5, 0.5) }, // 涟漪中心位于屏幕中心
             uRadius: { value: 20.0 }, // 涟漪的半径
-            uAmplitude: { value: 50 }, // 涟漪的振幅
             u_color: {
                 value: new THREE.Color('#f00')
             },
@@ -62,7 +59,21 @@ export default function (option = {}) {
             uTime: { value: 0.5 }, // 初始时间值
             uCenter: { value: new THREE.Vector2(0.5, 0.5) }, // 涟漪中心位于屏幕中心
             uRadius: { value: 20.0 }, // 涟漪的半径
-            uAmplitude: { value: 50 }, // 涟漪的振幅
+            u_color: {
+                value: new THREE.Color('#f00')
+            },
+        },
+        blending: THREE.NormalBlending,
+        transparent: true,
+        depthWrite:false
+    });
+    const shaderMaterial3 = new THREE.ShaderMaterial({
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        uniforms: {
+            uTime: { value: 0.75 }, // 初始时间值
+            uCenter: { value: new THREE.Vector2(0.5, 0.5) }, // 涟漪中心位于屏幕中心
+            uRadius: { value: 20.0 }, // 涟漪的半径
             u_color: {
                 value: new THREE.Color('#f00')
             },
@@ -74,16 +85,21 @@ export default function (option = {}) {
     const geometry = new THREE.CircleGeometry(50, 50, 1, Math.PI * 2); // 10x10的网格，1x1的分段  
     const plane = new THREE.Mesh(geometry, shaderMaterial);
     const plane2 = new THREE.Mesh(geometry, shaderMaterial2);
+    const plane3 = new THREE.Mesh(geometry, shaderMaterial3);
     plane.rotation.x = - Math.PI / 2; // 将平面旋转90度，使其与XZ平面对齐  
     plane.position.copy(newOption.position)
     plane2.rotation.x = - Math.PI / 2; // 将平面旋转90度，使其与XZ平面对齐  
     plane2.position.copy(newOption.position)
+    plane3.rotation.x = - Math.PI / 2; // 将平面旋转90度，使其与XZ平面对齐  
+    plane3.position.copy(newOption.position)
     const group = new THREE.Group()
     group.add(plane)
     group.add(plane2)
+    group.add(plane3)
     const animationFn = () => {
         shaderMaterial.uniforms.uTime.value += 0.01
         shaderMaterial2.uniforms.uTime.value += 0.01
+        shaderMaterial3.uniforms.uTime.value += 0.01
         requestAnimationFrame(animationFn)
     }
     animationFn()
